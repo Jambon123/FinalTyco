1365fffe7b1733ac67ed293366fe4a2596b6140c
Jambon123 <68718646+Jambon123@users.noreply.github.com>
2026-05-13 19:34:46 +0100

    AE2 Quest Progress

--- kubejs/server_scripts/mods/AE2.js
+++ kubejs/server_scripts/mods/AE2.js
@@ -3,4 +3,5 @@ ServerEvents.recipes(event => {
     const gtRecipe = global.pvHelpers.easyGTRecipe(event)
 
     event.replaceInput({mod: "ae2", input:"#forge:ingots/iron"}, "#forge:ingots/iron", "gtceu:steel_ingot")
+    event.replaceInput({id: "ae2:wireless_part"}, "ae2:fluid_pearl", "ae2:fluix_crystal")
 })
