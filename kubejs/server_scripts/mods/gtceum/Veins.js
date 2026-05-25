242514b462ae3b563ffc82dee89daf1befe8f7b2
Nicolas Garlinski <81200460+intothenextportal@users.noreply.github.com>
2026-05-25 15:28:28 +0200

    moon ore vein test

--- kubejs/server_scripts/mods/gtceum/Veins.js
+++ kubejs/server_scripts/mods/gtceum/Veins.js
@@ -1,7 +1,21 @@
 //priority: 5
 GTCEuServerEvents.oreVeins(event => {
 //NOTE: 1-Terth
-
+        public static final GTOreDefinition MAGNETITE_VEIN = create("magnetite_vein", vein -> vein
+            .clusterSize(UniformInt.of(38, 44)).density(0.15f).weight(30)
+            .layer(WorldGenLayers.Moon_Stone)
+            .heightRangeUniform(20, 80)
+            .biomes(BiomeTags.IS_MOON)
+            .layeredVeinGenerator(generator -> generator
+                    .withLayerPattern(() -> GTLayerPattern.builder(OVERWORLD_RULES)
+                            .layer(l -> l.weight(3).mat(Magnetite).size(1, 4))
+                            .layer(l -> l.weight(2).mat(VanadiumMagnetite).size(1, 2))
+                            .layer(l -> l.weight(2).mat(Chromite).size(1, 1))
+                            .layer(l -> l.weight(1).mat(Gold).size(1, 1))
+                            .build()))
+            .surfaceIndicatorGenerator(indicator -> indicator
+                    .surfaceRock(Magnetite)
+                    .placement(SurfaceIndicatorGenerator.IndicatorPlacement.ABOVE)));
 
 //NOTE: 1.5-Orbit
 
