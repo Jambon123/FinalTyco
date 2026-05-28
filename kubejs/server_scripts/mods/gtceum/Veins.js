ff7cf78d66ea72a6da645ba5cb5c952ee302a867
Nicolas Garlinski <81200460+intothenextportal@users.noreply.github.com>
2026-05-28 17:41:11 +0200

    ore vein fix

--- kubejs/server_scripts/mods/gtceum/Veins.js
+++ kubejs/server_scripts/mods/gtceum/Veins.js
@@ -5,31 +5,44 @@ GTCEuStartupEvents.registry('gtceu:world_gen_layer', event => {
         .dimensions('genesis:moon') // [*]
 })  
 
-GTCEuServerEvents.oreVeins(event => {
 //NOTE: 1-Terth
 // 
-        public static final GTOreDefinition MAGNETITE_VEIN = create("magnetite_vein", vein -> vein
-            .clusterSize(UniformInt.of(38, 44)).density(0.15f).weight(30)
-            .layer(WorldGenLayers.Moon_Stone)
-            vein.layer("Noom")
-            .heightRangeUniform(20, 80)
-            .biomes(BiomeTags.IS_MOON)
-            .layeredVeinGenerator(generator -> generator
-                    .withLayerPattern(() -> GTLayerPattern.builder(OVERWORLD_RULES)
-                            .layer(l -> l.weight(3).mat(Magnetite).size(1, 4))
-                            .layer(l -> l.weight(2).mat(VanadiumMagnetite).size(1, 2))
-                            .layer(l -> l.weight(2).mat(Chromite).size(1, 1))
-                            .layer(l -> l.weight(1).mat(Gold).size(1, 1))
-                            .build()))
-            .surfaceIndicatorGenerator(indicator -> indicator
-                    .surfaceRock(Magnetite)
-                    .placement(SurfaceIndicatorGenerator.IndicatorPlacement.ABOVE)));
 
 //NOTE: 1.5-Orbit
 
 
 //NOTE: 2: Noom
+GTCEuServerEvents.oreVeins(event => {
+    event.add("kubejs:magnetitemoon_vein", vein => {
+        // Basic vein generation properties
+        vein.weight(200) // [*] 
+        vein.clusterSize(40) // [*] 
+        vein.density(0.25) // [*] 
+        vein.discardChanceOnAirExposure(0) // 
 
+        // Define where the vein can generate
+        vein.layer("Noom") // [*] 
+        vein.dimensions("genesis:moon") // 
+
+        // Define a height range:
+        // You must choose EXACTLY ONE of these options! [*]
+        vein.heightRangeUniform(-60, 20) // 
+        vein.heightRangeTriangle(-60, 20) // 
+        vein.heightRange(/* ... */) // 
+
+        // Define the vein's generator:
+    vein.layeredVeinGenerator(generator => generator
+    .buildLayerPattern(pattern => pattern
+        .layer(l => l.weight(3).mat(GTMaterials.Magnetite).size(2, 4))
+        .layer(l => l.weight(2).mat(GTMaterials.Gold).size(1, 1))
+        .layer(l => l.weight(1).block(() => Block.getBlock('minecraft:oak_log')).size(1, 1))
+        .layer(l => l.weight(1).state(() => Block.getBlock('minecraft:oak_planks').defaultBlockState()).size(1, 1))
+    )
+)
+        // Add one or more type of surface indicator to the vein:
+        vein.addIndicator(magnetite) // 
+    })
+})
 
 //NOTE: 3: Glugg
 
